6e267dac90b77ea8d7bfd4cfc477d113e474c67a
home-serve <home-serve>
2020-03-28 19:13:58 -0400

    fix deploy

--- rebuild-deploy.sh
+++ rebuild-deploy.sh
@@ -3,7 +3,7 @@
 git pull origin feature/docker
 GIT_HASH=$(git rev-parse --short HEAD)
 
-docker build -t ws-base:$GIT_HASH docker-base/
+docker build -t ws:base docker-base/
 
 # docker run --rm -it -v $PWD:/app ws:base bundle install --path /app
 # docker run --rm -it -v $PWD:/app ws:base yarn install
--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 GIT_HASH=$(git rev-parse --short HEAD)
-
+cp -pvr .current_version .previous_version
 docker rm -f ws
-docker run -d -it --name ws -v $PWD/db:/app/db -p 3000:3000 ws:$GIT_HASH
+docker run -d -it --name ws -v $PWD/db:/app/db -p 3000:3000 ws:$GIT_HASH && echo $GIT_HASH > .current_version
