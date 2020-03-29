ca1cd5b5dad98d1fe425c326680683039f102fe4
home-serve <home-serve>
2020-03-29 12:28:58 -0400

    update restart

--- redeploy.sh
+++ redeploy.sh
@@ -3,4 +3,4 @@
 GIT_HASH=$(git rev-parse --short HEAD)
 cp -pvr .current_version .previous_version
 docker rm -f ws
-docker run -d -it --name ws -v $PWD/db:/app/db -p 3000:3000 --always ws:$GIT_HASH && echo $GIT_HASH > .current_version
+docker run -d -it --name ws -v $PWD/db:/app/db -p 3000:3000 --restart=always ws:$GIT_HASH && echo $GIT_HASH > .current_version
