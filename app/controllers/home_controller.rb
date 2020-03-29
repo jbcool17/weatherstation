b887f9f56cd0d7c4f7a8e25933362004c9a80629
John Brilla <jbrilla17@gmail.com>
2020-03-29 13:22:09 -0400

    add live page no auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index,:live]
 
   # Main Home Page
   def index
